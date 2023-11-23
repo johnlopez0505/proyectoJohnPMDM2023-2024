58d9f0907a7bd7b6700e8729f7ce728d3b73eb23
john-lopez <lopezcon1@hotmail.com>
2023-11-23 01:24:17 +0100

    version 0.0.2

--- app/src/main/java/com/john/proyectojohnpmdm2023_2024/Principal.kt
+++ app/src/main/java/com/john/proyectojohnpmdm2023_2024/Principal.kt
@@ -2,10 +2,43 @@ package com.john.proyectojohnpmdm2023_2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import com.john.proyectojohnpmdm2023_2024.databinding.ActivityPrincipalBinding
 
 class Principal : AppCompatActivity() {
+    private lateinit var principalBinding: ActivityPrincipalBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        principalBinding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(principalBinding.root)
+
+        initHander()
+        login()
+    }
+
+    private fun initHander() {
+        var handler = Handler(Looper.getMainLooper()) //queremos que el tema de la IU, la llevemos al hilo principal.
+        principalBinding.progressCircular.visibility = View.VISIBLE  //hacemos visible el progress
+        principalBinding.cardview.visibility =   View.GONE //ocultamos el cardview.
+        Thread{
+            Thread.sleep( 2000)
+            handler.post{
+                principalBinding.progressCircular.visibility = View.GONE //ocultamos el progress
+
+                principalBinding.cardview.visibility = View.VISIBLE
+                Toast.makeText(this, "EStamos en la pantalla principal",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+    private fun login() {
+        val txtName = principalBinding.textnombre
+        val name = getIntent().getStringExtra("name")
+        if (name!= null){
+            txtName.text = "$name"
+        }
     }
 }
